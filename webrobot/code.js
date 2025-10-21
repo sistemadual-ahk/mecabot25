d9eed299f660523647014f6c7bf43bd069de75c7
OvandoEskere <100881107+OvandoEskere@users.noreply.github.com>
2025-10-21 09:57:04 -0300

    Update code.js

--- webrobot/code.js
+++ webrobot/code.js
@@ -1,6 +1,6 @@
 // Crear conexión WebSocket con Node-RED
 // Por defecto Node-RED expone ws://localhost:1880/ws/<path>
-const socket = new WebSocket("https://localhost:1880/comando");
+const socket = new WebSocket("https://192.168.2.150:1880/code");
 
 // Evento: cuando se abre la conexión
 socket.onopen = () => {
@@ -49,4 +49,5 @@ document.getElementById("vacio4").addEventListener("pointerdown", () => enviarCo
 document.getElementById("vacio5").addEventListener("pointerdown", () => enviarComando("vacio"));
 
 document.getElementById("vacio6").addEventListener("pointerdown", () => enviarComando("vacio"));
+
 
