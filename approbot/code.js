402f52982f954edb632c0adf504a4d618cf62ac2
AleMikael03 <alemikael2025@gmail.com>
2025-10-31 12:19:36 -0300

    Update code.js

--- approbot/code.js
+++ approbot/code.js
@@ -19,7 +19,7 @@ socket.onmessage = (event) => {
 // Función para enviar el comando
 function enviarComando(comando) {
   if (socket.readyState === WebSocket.OPEN) {
-    socket.send(comando);
+       socket.send(JSON.stringify({comando}));
     console.log("Enviado comando:", comando);
   } else {
     console.error("El WebSocket no está conectado");
